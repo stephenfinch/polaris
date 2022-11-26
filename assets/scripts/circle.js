8ff8a060a0b385de6999158ea39aa3169dcfb39b
Stephen Finch <smfinch99@gmail.com>
2022-11-25 19:33:54 -0500

    remove bullet after hit

--- assets/scripts/circle.js
+++ assets/scripts/circle.js
@@ -55,8 +55,9 @@ class Polaris extends Circle {
   }
 
   shoot(target) {
+    const bullet = new Bullet(target)
+    this.bullets.push(bullet)
     this._readyToShoot = false
-    this.bullets.push(new Bullet(target))
   }
 
   updateBullets() {
@@ -94,13 +95,31 @@ class Bullet extends Circle {
     this.pos = createVector(width / 2, height / 2)
     this.r = 2
     this.target = target
+    this.damage = 1
+    this.active = true
   }
 
-  update() {
+  checkForHit() {
+    if (this.collision(this.target.pos.x, this.target.pos.y, this.target.r, this.r)) {
+      this.target.takeHit(this.damage)
+      this.active = false
+    }
+  }
+
+  move() {
     const setpoint = this.target.pos.copy()
     const vel = setpoint.sub(this.pos)
     vel.setMag(3)
     this.pos.add(vel)
+    this.checkForHit()
+  }
+
+  fade() {
+    this.circleDiv.remove()
+  }
+
+  update() {
+    this.active ? this.move() : this.fade()
   }
 
   show() {
@@ -111,16 +130,45 @@ class Bullet extends Circle {
 class Ship extends Circle {
   constructor() {
     super()
+    this.active = true
     this.circleDiv.addClass('ship')
     this.pos = createVector(random(0, width), random(0, height))
     this.r = 7
+    this.health = 1
+    this.fadeValue = 1
   }
 
-  update() {
+  takeHit(damage) {
+    this.health -= damage
+    if (this.health <= 0) this.active = false
+  }
+
+  move() {
     const center = createVector(width / 2, height / 2)
     const vel = center.sub(this.pos)
     vel.setMag(1)
     this.pos.add(vel)
+  }
+
+  fade() {
+    if (this.dead()) return
+
+    this.fadeValue -= 0.1
+    this.circleDiv.style('background', `rgba(215, 65, 167, ${this.fadeValue / 4})`)
+    this.circleDiv.style('border', `2px solid rgba(215, 65, 167, ${this.fadeValue})`)
+    this.circleDiv.style('box-shadow', `0px 0px 25px 10px rgba(215, 65, 167, ${this.fadeValue / 4})`)
+    this.circleDiv.style('width', `${34 - this.fadeValue * 20}px`)
+    this.circleDiv.style('height', `${34 - this.fadeValue * 20}px`)
+  }
+
+  dead() {
+    return this.fadeValue < 0
+  }
+
+  update() {
+    this.active ? this.move() : this.fade()
+
+    if (this.dead()) this.circleDiv.remove()
   }
 
   show() {
